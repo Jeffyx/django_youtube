ad917eaa541caea3b25d58b63f688f2e7e69e8e5
Jeff <jeff8er@aol.com>
2021-01-19 15:15:57 -0500

    server 500 issue

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '383vv&y-9pcf7v3tunu=6wwk3(c3i)5-1m58t9!4^8myf=4i6s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['django-youtube.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ["django-youtube.herokuapp.com", "127.0.0.1", "73.10.19.197"]
 
 
 # Application definition
